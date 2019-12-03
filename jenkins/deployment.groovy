76ee52237493abd87da100f3da2ff8ecb960df7d
Aron Geerts <aron.geerts@nike.com>
2019-12-03 16:36:41 +0100

    add pipeline for deployment unit testing

---
+++ jenkins/deployment.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    stages {
+        stage('Get source code') {
+            steps {
+                dir('/var/jenkins_home/workspace'){
+                    sh """
+                    rm -rf automated_testing || true
+                    git clone https://github.com/arongeerts/automated_testing.git
+                    """
+                }
+            }
+        }
+        stage('Test') {
+            steps {
+                dir('/var/jenkins_home/workspace/automated_testing/python_tests') {
+                    sh """
+                    python ./unit_test.py
+                    """
+                }
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh 'echo "do deployment'
+            }
+        }
+    }
+}
---
+++ python_tests/unit_test.py
@@ -0,0 +1,5 @@
+def test_dummy():
+    assert False
+
+if __name__ == '__main__':
+    test_dummy()
