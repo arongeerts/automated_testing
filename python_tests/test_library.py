da9e914145d9313e2fc11457b2807266a1e45ca2
Aron Geerts <aron.geerts@nike.com>
2019-12-03 10:54:06 +0100

    test ranges

--- python_tests/test_library.py
+++ python_tests/test_library.py
@@ -2,6 +2,7 @@ from db_connection import get_conn
 
 db_connection = get_conn()
 
+
 def execute_function(function_name, params):
     return globals()[function_name](params)
 
@@ -13,6 +14,7 @@ def test_not_null(params):
     stmt = 'SELECT COUNT(*) FROM {}.{} WHERE {} IS NULL'.format(database, table, field)
     __assert_count_zero(stmt)
 
+
 def test_timestamp_order(params):
     database = params['DB']
     table = params['TABLE']
@@ -21,6 +23,18 @@ def test_timestamp_order(params):
     stmt = 'SELECT COUNT(*) FROM {db}.{tab} WHERE {f1} IS NOT NULL AND {f2} IS NOT NULL AND {f1} > {f2}'.format(
         db=database, tab=table, f1=field1, f2=field2)
     __assert_count_zero(stmt)
+
+
+def test_field_in_range(params):
+    database = params['DB']
+    table = params['TABLE']
+    field = params['FIELD']
+    low = params.get('LOW', '- POWER(2,31)+1')
+    high = params.get('HIGH', 'POWER(2,31)-1')
+    stmt = 'SELECT COUNT(*) FROM {db}.{tab} WHERE {f} IS NOT NULL and ({f} > {high} OR {f} < {low})'.format(
+        db=database, tab=table, f=field, low=low, high=high)
+    __assert_count_zero(stmt)
+
 
 def __assert_count_zero(stmt):
     cursor = db_connection.cursor()
