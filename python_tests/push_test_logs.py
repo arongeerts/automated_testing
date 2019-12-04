1acdb99d38049b841a907672d89eb20ef289eb51
Aron Geerts <aron.geerts@nike.com>
2019-12-04 16:47:57 +0100

    fix endpoint

--- python_tests/push_test_logs.py
+++ python_tests/push_test_logs.py
@@ -2,7 +2,7 @@ import boto3, datetime
 
 f = open('test_output.txt')
 folder_key = 'TEST_OUTPUT_' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-s3 = boto3.client('s3', endpoint_url='localhost:5002')
+s3 = boto3.client('s3', endpoint_url='http://localhost:5002')
 
 try:
     s3.create_bucket(ACL='public-read-write',
