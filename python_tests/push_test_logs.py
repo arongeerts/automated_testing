bb5b52c29123c19a75bf4ca2698f04e64e737a9c
Aron Geerts <aron.geerts@nike.com>
2019-12-05 08:59:37 +0100

    try

--- python_tests/push_test_logs.py
+++ python_tests/push_test_logs.py
@@ -3,14 +3,16 @@ import boto3, datetime
 f = open('test_output.txt')
 folder_key = 'TEST_OUTPUT_' + datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 s3 = boto3.client('s3',
-                  endpoint_url='http://localhost:5002',
+                  endpoint_url='http://localstack-s3:5002',
                   aws_access_key_id='foo',
                   aws_secret_access_key='bar')
 
 try:
     s3.create_bucket(ACL='public-read-write',
                      Bucket='mybucket')
+    print('created bucket mybucket')
 except:
-    pass
+    print('bucket mybucket already exists')
+
 
 s3.put_object(Bucket='mybucket', Key=folder_key, Body=f)
